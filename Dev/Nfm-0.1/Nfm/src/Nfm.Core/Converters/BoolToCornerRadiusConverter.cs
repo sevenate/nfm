0855e8b69bdd328ae36ad890ad50f4c7f9df66fa
Sevenate <1175845+sevenate@users.noreply.github.com>
2009-03-27 23:06:33 +0000

    Add experimental "neon background" for custom file types.

---
+++ Dev/Nfm-0.1/Nfm/src/Nfm.Core/Converters/BoolToCornerRadiusConverter.cs
@@ -0,0 +1,59 @@
+// <copyright file="BoolToCornerRadiusConverter.cs" company="HD">
+// 	Copyright (c) 2009 HD. All rights reserved.
+// </copyright>
+// <author name="Andrew Levshoff">
+// 	<email>alevshoff@hd.com</email>
+// 	<date>2009-03-27</date>
+// </author>
+// <editor name="Andrew Levshoff">
+// 	<email>alevshoff@hd.com</email>
+// 	<date>2009-03-27</date>
+// </editor>
+// <summary>Convert boolean value to corner radius of <see cref="Border"/>.</summary>
+
+using System;
+using System.Globalization;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace Nfm.Core.Converters
+{
+	/// <summary>
+	/// Convert boolean value to corner radius of <see cref="Border"/>.
+	/// </summary>
+	[ValueConversion(typeof (bool), typeof (int))]
+	public class BoolToCornerRadiusConverter : IValueConverter
+	{
+		#region Implementation of IValueConverter
+
+		/// <summary>
+		/// Converts a value. 
+		/// </summary>
+		/// <returns>A converted value. If the method returns null, the valid null value is used.</returns>
+		/// <param name="value">The value produced by the binding source.</param>
+		/// <param name="targetType">The type of the binding target property.</param>
+		/// <param name="parameter">The converter parameter to use.</param>
+		/// <param name="culture">The culture to use in the converter.</param>
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return value != null && (bool) value
+			       	? 0
+			       	: 10;
+		}
+
+		/// <summary>
+		/// Converts a value. 
+		/// </summary>
+		/// <returns>A converted value. If the method returns null, the valid null value is used.</returns>
+		/// <param name="value">The value that is produced by the binding target.</param>
+		/// <param name="targetType">The type to convert to.</param>
+		/// <param name="parameter">The converter parameter to use.</param>
+		/// <param name="culture">The culture to use in the converter.</param>
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotSupportedException();
+		}
+
+		#endregion
+	}
+}
