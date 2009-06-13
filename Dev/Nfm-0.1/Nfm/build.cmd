9ec7182a68976b16a01f18705b21bef0a92ec35b
Sevenate <1175845+sevenate@users.noreply.github.com>
2009-06-13 11:18:36 +0000

    Switched to Caliburn @25092 changeset.

--- Dev/Nfm-0.1/Nfm/build.cmd
+++ Dev/Nfm-0.1/Nfm/build.cmd
@@ -1,5 +1,5 @@
 @rem -- Prevent creating "Nfm.sln.cache" file --
 @set MSBuildUseNoSolutionCache=1
 
-@c:\WINDOWS\Microsoft.NET\Framework\v3.5\MSBuild.exe /t:Clean /p:Configuration=Release Nfm.sln
+rem @c:\WINDOWS\Microsoft.NET\Framework\v3.5\MSBuild.exe /t:Clean /p:Configuration=Release Nfm.sln
 @c:\WINDOWS\Microsoft.NET\Framework\v3.5\MSBuild.exe /t:ReBuild /p:Configuration=Release Nfm.sln
