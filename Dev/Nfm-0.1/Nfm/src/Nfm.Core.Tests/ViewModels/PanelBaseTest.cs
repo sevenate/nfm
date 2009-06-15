a4bfd112f494219ad905b7dd3a55da9a04acfc26
Sevenate <1175845+sevenate@users.noreply.github.com>
2009-06-15 17:08:16 +0000

    Rollback to xUnit 1.1 (ReSharper Test Runner from xUnit Contrib did't support 1.5CTP yet).

---
+++ Dev/Nfm-0.1/Nfm/src/Nfm.Core.Tests/ViewModels/PanelBaseTest.cs
@@ -0,0 +1,41 @@
+// <copyright file="PanelBaseTest.cs" company="HD">
+// 	Copyright (c) 2009 HD. All rights reserved.
+// </copyright>
+// <author name="Andrew Levshoff">
+// 	<email>alevshoff@hd.com</email>
+// 	<date>2009-06-15</date>
+// </author>
+// <editor name="Andrew Levshoff">
+// 	<email>alevshoff@hd.com</email>
+// 	<date>2009-06-15</date>
+// </editor>
+// <summary>Unit tests for <see cref="PanelBase"/>.</summary>
+
+using Moq;
+using Nfm.Core.ViewModels;
+using Xunit;
+
+namespace Nfm.Core.Tests.ViewModels
+{
+	/// <summary>
+	/// Unit tests for <see cref="PanelBase"/> class.
+	/// </summary>
+	public class PanelBaseTest
+	{
+		[Fact]
+		public void Clone()
+		{
+			var panelContentMoq = new Mock<IPanelContent>();
+			panelContentMoq.Setup(pc => pc.Clone()).Returns(new Mock<IPanelContent>().Object);
+
+			var panelBase = new PanelBase
+			                {
+								PanelContent = panelContentMoq.Object
+			                };
+
+			var clonePanelBase = (PanelBase)panelBase.Clone();
+
+			Assert.Equal(clonePanelBase.IsSelected, panelBase.IsSelected);
+		}
+	}
+}
