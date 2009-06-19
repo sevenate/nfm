189b43e110a7797b2e04cd27bf6e2e3a1dc4602a
Sevenate <1175845+sevenate@users.noreply.github.com>
2009-06-19 05:26:43 +0000

    Minor icons refactoring.

--- Dev/Nfm-0.1/Nfm/src/Nfm.Core/Resources/Icons/TriangleArrow.xaml.cs
+++ Dev/Nfm-0.1/Nfm/src/Nfm.Core/Resources/Icons/TriangleArrow.xaml.cs
@@ -1,23 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-namespace Nfm.Core.Resources.Icons
+﻿namespace Nfm.Core.Resources.Icons
 {
 	/// <summary>
 	/// Interaction logic for TriangleArrow.xaml
 	/// </summary>
-	public partial class TriangleArrow : UserControl
+	public partial class TriangleArrow
 	{
 		public TriangleArrow()
 		{
