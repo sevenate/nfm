ddee16f69963ec7cd3b815529f412e92d1873fbe
Sevenate <1175845+sevenate@users.noreply.github.com>
2009-06-22 23:01:28 +0000

    Removed AddInManager.cs and AddInManagerTest.cs from project files; Updated assembly properties to 0.2 version; Added: pause in the end of release build script.

--- Dev/Nfm-0.2/Nfm/build.cmd
+++ Dev/Nfm-0.2/Nfm/build.cmd
@@ -1,5 +1,8 @@
-@rem -- Prevent creating "Nfm.sln.cache" file --
-@set MSBuildUseNoSolutionCache=1
+@echo off
 
-rem @c:\WINDOWS\Microsoft.NET\Framework\v3.5\MSBuild.exe /t:Clean /p:Configuration=Release Nfm.sln
-@c:\WINDOWS\Microsoft.NET\Framework\v3.5\MSBuild.exe /t:ReBuild /p:Configuration=Release Nfm.sln
+rem -- Prevent creating "Nfm.sln.cache" file --
+set MSBuildUseNoSolutionCache=1
+
+rem c:\WINDOWS\Microsoft.NET\Framework\v3.5\MSBuild.exe /t:Clean /p:Configuration=Release Nfm.sln
+c:\WINDOWS\Microsoft.NET\Framework\v3.5\MSBuild.exe /t:ReBuild /p:Configuration=Release Nfm.sln
+pause
