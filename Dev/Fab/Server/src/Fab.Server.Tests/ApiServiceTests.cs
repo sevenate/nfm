4d24005c6bdfdd51195d26f1d11e6baf79a0b26a
Sevenate <1175845+sevenate@users.noreply.github.com>
2010-01-30 12:26:10 +0000

    Add unit tests (xUnit + Moq) project for Fab.Server.

---
+++ Dev/Fab/Server/src/Fab.Server.Tests/ApiServiceTests.cs
@@ -0,0 +1,25 @@
+// <copyright file="ApiServiceTests.cs" company="HD">
+// 	Copyright (c) 2010 HD. All rights reserved.
+// </copyright>
+// <author name="Andrew Levshoff">
+// 	<email>alevshoff@hd.com</email>
+// 	<date>2010-01-30</date>
+// </author>
+// <editor name="Andrew Levshoff">
+// 	<email>alevshoff@hd.com</email>
+// 	<date>2010-01-30</date>
+// </editor>
+// <summary>Unit tests for <see cref="ApiService" />.</summary>
+
+using Fab.Server;
+
+namespace Fab.Server.Tests
+{
+    /// <summary>
+    /// Unit tests for <see cref="ApiService" />.
+    /// </summary>
+    public class ApiServiceTests
+    {
+        
+    }
+}
