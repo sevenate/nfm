c0764211a264dc5b17b28a48a11c05bd970ebce2
Sevenate <1175845+sevenate@users.noreply.github.com>
2010-03-14 21:56:34 +0000

    Add basic silverlight client for FAB service.

---
+++ Dev/Fab/Client/Silverlight/src/Fab.Client/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+using Fab.Client.ApiServiceReference;
+
+namespace Fab.Client
+{
+	/// <summary>
+	/// The main page.
+	/// </summary>
+	public partial class MainPage
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="MainPage"/> class.
+		/// </summary>
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		private void TestButton_Click(object sender, RoutedEventArgs e)
+		{
+			ResultText.Text = "Connecting...";
+
+			var proxy = new UserServiceClient();
+			proxy.IsLoginAvailableCompleted += ProxyOnIsLoginAvailableCompleted;
+			proxy.IsLoginAvailableAsync("123456");
+		}
+		
+		private void ProxyOnIsLoginAvailableCompleted(
+			object sender, IsLoginAvailableCompletedEventArgs isLoginAvailableCompletedEventArgs)
+		{
+			if (isLoginAvailableCompletedEventArgs.Error != null)
+			{
+				ResultText.Text = "Error: " + isLoginAvailableCompletedEventArgs.Error;
+			}
+			else
+			{
+				ResultText.Text = "Done. Connection is OK";
+			}
+		}
+	}
+}
