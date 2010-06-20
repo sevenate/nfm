699fb8923f9fc45e1bf9fbc2736444345af0b15c
Sevenate <1175845+sevenate@users.noreply.github.com>
2010-06-20 22:33:19 +0000

    Update xUnit to 1.6 version. Update Moq to latest version.

---
+++ Dev/Fab/Server/tools/run.cmd
@@ -0,0 +1,8 @@
+@echo off
+
+rem -- Run tests with msbuild --
+
+rem More info - http://xunit.codeplex.com/Wiki/View.aspx?title=HowToUseMSBuild
+
+c:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe run.targets
+pause
